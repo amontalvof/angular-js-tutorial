f85e2cf6b2b910b406229aa0983e7b6ea8f4fa6d
amontalvof <amontalvof92@gmail.com>
2022-02-18 09:03:48 -0600

    feat: ng-show directive

--- app/app.js
+++ app/app.js
@@ -4,10 +4,10 @@ myNinjaApp.controller('NinjaController', [
     '$scope',
     function ($scope) {
         $scope.ninjas = [
-            { name: 'yoshi', belt: 'green', rate: 50 },
-            { name: 'crystal', belt: 'yellow', rate: 30 },
-            { name: 'ryu', belt: 'orange', rate: 10 },
-            { name: 'shaun', belt: 'black', rate: 1000 },
+            { name: 'yoshi', belt: 'green', rate: 50, available: true },
+            { name: 'crystal', belt: 'yellow', rate: 30, available: false },
+            { name: 'ryu', belt: 'orange', rate: 10, available: false },
+            { name: 'shaun', belt: 'black', rate: 1000, available: true },
         ];
     },
 ]);
