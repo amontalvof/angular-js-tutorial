f7af8451f4b4e598b4dd029c18f39659b002dd6f
amontalvof <amontalvof92@gmail.com>
2022-02-19 13:15:34 -0600

    feat: ng-click directive

--- app/app.js
+++ app/app.js
@@ -3,9 +3,14 @@ const myNinjaApp = angular.module('myNinjaApp', []);
 myNinjaApp.controller('NinjaController', [
     '$scope',
     function ($scope) {
+        $scope.removeNinja = function (ninja) {
+            var removedNinja = $scope.ninjas.indexOf(ninja);
+            $scope.ninjas.splice(removedNinja, 1);
+        };
+
         $scope.ninjas = [
             { name: 'yoshi', belt: 'green', rate: 50, available: true },
-            { name: 'crystal', belt: 'yellow', rate: 30, available: false },
+            { name: 'crystal', belt: 'yellow', rate: 30, available: true },
             { name: 'ryu', belt: 'orange', rate: 10, available: false },
             { name: 'shaun', belt: 'black', rate: 1000, available: true },
         ];
