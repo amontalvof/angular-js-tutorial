5b01b1a2e6282e5fcc32bc7fbfe9c08030d14776
amontalvof <amontalvof92@gmail.com>
2022-02-28 09:06:13 -0600

    feat: Transclude & Replace

--- app/app.js
+++ app/app.js
@@ -25,6 +25,8 @@ myNinjaApp.directive('randomNinja', [
                 title: '=',
             },
             templateUrl: 'views/random.html',
+            transclude: true,
+            replace: true,
             controller: function ($scope) {
                 $scope.random = Math.floor(Math.random() * 4);
             },
